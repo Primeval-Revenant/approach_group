a49fc8714ec387cf677dd474dc46e6a9d0963cc6
Primeval-Revenant <ricarte.ribeiro@tecnico.ulisboa.pt>
2022-08-26 19:51:24 +0100

    Start of dynamic approach
    Removed the wait for result so as to allow for dynamic approaches and added a component to notify the people_publisher of which group should be altered for approach.

--- scripts/Backup version/approach_group.py
+++ scripts/Backup version/approach_group.py
@@ -11,7 +11,6 @@ from nav_msgs.msg import OccupancyGrid
 from group_msgs.msg import People, Groups
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import Pose, PoseArray, PointStamped
-from move_base_msgs.msg import MoveBaseActionResult
 from ellipse import plot_ellipse
 from sys import getsizeof, argv
 import matplotlib.pyplot as plt
@@ -60,13 +59,12 @@ def movebase_client(goal_pose, goal_quaternion):
     goal.target_pose.pose.orientation.w = goal_quaternion[3]
 
     client.send_goal(goal)
-    #wait = client.wait_for_result()
-    # if not wait:
-    #     rospy.logerr("Action server not available!")
-    #     rospy.signal_shutdown("Action server not available!")
-    # else:
-    
-    return client.get_result()
+    wait = client.wait_for_result()
+    if not wait:
+        rospy.logerr("Action server not available!")
+        rospy.signal_shutdown("Action server not available!")
+    else:
+        return client.get_result()
 
 
 def group_radius(persons, group_pose):
@@ -114,8 +112,6 @@ class ApproachingPose():
         rospy.init_node('ApproachPose', anonymous=True)
         rospy.Subscriber("/groups",Groups , self.callbackGr, queue_size=1)
         rospy.Subscriber("/clicked_point",PointStamped, self.callbackPoint, queue_size=1)
-        rospy.Subscriber("/move_base/result",MoveBaseActionResult, self.callbackMoveResult, queue_size=1)
-        self.pub = rospy.Publisher('/approach_target', PointStamped, queue_size=1)
         self.loop_rate = rospy.Rate(rospy.get_param('~loop_rate', 10.0))
 
         self.costmap_received = False
@@ -124,39 +120,12 @@ class ApproachingPose():
         self.pose = []
         self.point_clicked = []
         self.groups_data = []
-        self.moveresult = None
-        self.target = None
         self.plotting = False
 
     def callbackGr(self,data):
         """ Groups data callback. """
         self.people_received = True
         self.groups_data = data
-
-        self.groups = []
-        for group in self.groups_data.groups:
-            tmp_group = []
-
-            if len(group.people) > 1: # Only store groups, ignore individuals
-                for people in group.people:
-    
-                    pose_x = people.position.x
-                    pose_y = people.position.y
-                    pose_yaw = people.orientation 
-
-
-                    if people.ospace:
-                        # group_radius average of the distance of the group members to the center
-                        # pspace_radius  Based on the closest person to the group center
-                        # ospace_radius Based on the farthest persons to the group center
-                        g_radius, pspace_radius, ospace_radius = group_radius(tmp_group, [pose_x, pose_y,pose_yaw])
-                        self.groups.append({'members': tmp_group,'pose':[pose_x, pose_y,pose_yaw], 'parameters' :[people.sx, people.sy], 'g_radius': g_radius, 'ospace_radius': ospace_radius, 'pspace_radius': pspace_radius})
-                    else:
-                        tmp_group.append([pose_x, pose_y, pose_yaw])
-            else:
-                for people in group.people:
-                    tmp_group.append([people.position.x, people.position.y, people.orientation])
-                    self.groups.append({'members': tmp_group, 'pose': [people.position.x, people.position.y, people.orientation],'parameters' :[people.sx, people.sy]})
      
     def callbackPoint(self,data):
         self.point_clicked = data
@@ -166,9 +135,6 @@ class ApproachingPose():
         """ Costmap Callback. """
         self.costmap = data
         self.costmap_received = True
-
-    def callbackMoveResult(self,data):
-        self.moveresult = data
 
            
     def run_behavior(self):
@@ -196,35 +162,35 @@ class ApproachingPose():
 
                 self.people_received = False
 
-                map_subscriber = rospy.Subscriber("/move_base/global_costmap/costmap",OccupancyGrid , self.callbackCm, queue_size=1)
+                rospy.Subscriber("/move_base/global_costmap/costmap",OccupancyGrid , self.callbackCm, queue_size=1)
                 
                 if self.costmap_received and self.groups_data and self.point_clicked:
                     self.costmap_received = False
                     
-                    # self.groups = []
-                    # for group in self.groups_data.groups:
-                    #     tmp_group = []
+                    self.groups = []
+                    for group in self.groups_data.groups:
+                        tmp_group = []
 
-                    #     if len(group.people) > 1: # Only store groups, ignore individuals
-                    #         for people in group.people:
+                        if len(group.people) > 1: # Only store groups, ignore individuals
+                            for people in group.people:
                 
-                    #             pose_x = people.position.x
-                    #             pose_y = people.position.y
-                    #             pose_yaw = people.orientation 
+                                pose_x = people.position.x
+                                pose_y = people.position.y
+                                pose_yaw = people.orientation 
 
 
-                    #             if people.ospace:
-                    #                 # group_radius average of the distance of the group members to the center
-                    #                 # pspace_radius  Based on the closest person to the group center
-                    #                 # ospace_radius Based on the farthest persons to the group center
-                    #                 g_radius, pspace_radius, ospace_radius = group_radius(tmp_group, [pose_x, pose_y,pose_yaw])
-                    #                 self.groups.append({'members': tmp_group,'pose':[pose_x, pose_y,pose_yaw], 'parameters' :[people.sx, people.sy], 'g_radius': g_radius, 'ospace_radius': ospace_radius, 'pspace_radius': pspace_radius})
-                    #             else:
-                    #                 tmp_group.append([pose_x, pose_y, pose_yaw])
-                    #     else:
-                    #         for people in group.people:
-                    #             tmp_group.append([people.position.x, people.position.y, people.orientation])
-                    #             self.groups.append({'members': tmp_group, 'pose': [people.position.x, people.position.y, people.orientation],'parameters' :[people.sx, people.sy]})
+                                if people.ospace:
+                                    # group_radius average of the distance of the group members to the center
+                                    # pspace_radius  Based on the closest person to the group center
+                                    # ospace_radius Based on the farthest persons to the group center
+                                    g_radius, pspace_radius, ospace_radius = group_radius(tmp_group, [pose_x, pose_y,pose_yaw])
+                                    self.groups.append({'members': tmp_group,'pose':[pose_x, pose_y,pose_yaw], 'parameters' :[people.sx, people.sy], 'g_radius': g_radius, 'ospace_radius': ospace_radius, 'pspace_radius': pspace_radius})
+                                else:
+                                    tmp_group.append([pose_x, pose_y, pose_yaw])
+                        else:
+                            for people in group.people:
+                                tmp_group.append([people.position.x, people.position.y, people.orientation])
+                                self.groups.append({'members': tmp_group, 'pose': [people.position.x, people.position.y, people.orientation],'parameters' :[people.sx, people.sy]})
 
 
                     # Calculate the distances between the chosen point and every group
@@ -234,23 +200,14 @@ class ApproachingPose():
 
                     self.point_clicked = []
 
-                    while True:
+                    group_idx = np.argsort(dis)
 
-                        group_idx = np.argsort(dis)
+                    # Try to find an appropriate pose and approach a group, starting from the one closest to the chosen point
+                    for group_idxs in group_idx:
 
-                        # Try to find an appropriate pose and approach a group, starting from the one closest to the chosen point
-
-                        #rospy.loginfo("Trying group %d",group_idx[0])
+                        rospy.loginfo("Trying group %d",group_idxs)
                     
-                        group = self.groups[group_idx[0]]
-
-                        self.target = (group["pose"][0],group["pose"][1])
-
-                        targetsend = PointStamped()
-                        targetsend.header.frame_id = "/map"
-                        targetsend.point.x = group["pose"][0]
-                        targetsend.point.y = group["pose"][1]
-                        self.pub.publish(targetsend)
+                        group = self.groups[group_idxs]
                         
                         if len(group['members']) > 1:
 
@@ -275,22 +232,16 @@ class ApproachingPose():
                                 goal_pose = approaching_poses[idx][0:2]
                                 goal_quaternion = convert.transformations.quaternion_from_euler(0,0,approaching_poses[idx][2])
                                 try:
-                                    #rospy.loginfo("Approaching group!")
+                                    rospy.loginfo("Approaching group!")
                                     result = movebase_client(goal_pose, goal_quaternion)
-                                    if self.moveresult:
-                                        if self.moveresult.status.status == 3:
-                                            rospy.loginfo("Goal execution done!")
-                                            break
+                                    if result:
+                                        rospy.loginfo("Goal execution done!")
+                                        break
                                 except rospy.ROSInterruptException:
                                     rospy.loginfo("Navigation test finished.")
 
                         else:
                             rospy.loginfo("Impossible to approach group due to no approach poses.") 
-
-                        dis = []
-                        for idx,group in enumerate(self.groups):   
-                            dis.append(euclidean_distance(group["pose"][0],group["pose"][1],self.target[0], self.target[1]))
-                        
                     break
                             
                                 
@@ -299,20 +250,3 @@ if __name__ == '__main__':
     if len(argv) > 1 and argv[1] == 'print':
         approaching_pose.plotting = True
     approaching_pose.run_behavior()
-
-
-#Setup id - TODO
-#Setup node to transfer info between publisher and approach - DONE
-#Deal with groups and individuals - DONE
-#Prepare to test unregistering the costmap subscriber - best case scenario, it updates. - TODO
-#Dynamic move orders - Just remove wait result and rewrite the client function - DONE
-#Setup result positive as a stop condition? Prevent future infinite loop problems? - DONE
-#Figure out how else to apply ids - TODO
-
-
-#add center detection to approach target in people_publisher - DONE
-#iterate group center as clicked point to always keep awareness of which group to approach even if they move - DONE
-
-#change loop conditions. Current ones don't allow constant awareness of robot position. - TODO
-#Figure out why vizzy has dificulty navigating after goal moves. Check that goal is the correct one? - TODO
-#publish approach poses??? - TODO
