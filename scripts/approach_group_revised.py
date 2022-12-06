594de1d3d55c9050770f907951e174d634c79ae0
Primeval-Revenant <ricarte.ribeiro@tecnico.ulisboa.pt>
2022-12-06 17:38:06 +0000

    Update approach_group_revised.py

--- scripts/approach_group_revised.py
+++ scripts/approach_group_revised.py
@@ -262,8 +262,8 @@ class ApproachingPose():
                                     pspace_radius = group["pspace_radius"]
                                     ospace_radius = group["ospace_radius"]
                                 else:
-                                    g_radius = 1.3
-                                    pspace_radius = 1.4
+                                    g_radius = 0.9
+                                    pspace_radius = 1.2
                                     ospace_radius = 0.45
 
                                 #Calculate approaching poses
